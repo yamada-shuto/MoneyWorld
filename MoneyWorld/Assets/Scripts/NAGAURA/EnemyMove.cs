1db30f55c11331ea49169152a6d24535a9ab91ba
TSITCL\s162129 <taka1122judo@gmail.com>
2017-05-17 16:39:46 +0900

    がんばった

--- MoneyWorld/Assets/Scripts/NAGAURA/EnemyMove.cs
+++ MoneyWorld/Assets/Scripts/NAGAURA/EnemyMove.cs
@@ -5,39 +5,34 @@ using UnityEngine.AI;
 
 public class EnemyMove : MonoBehaviour
 {
-    // キャラクターコントローラー
-    //private CharacterController m_EnemyController;
-    // ナビゲーター
-    private UnityEngine.AI.NavMeshAgent m_Agent;
+    // エージェント
+    private NavMeshAgent m_Agent;
     // アニメーター
     private Animator m_Animator;
-    // 歩く速度
-    public float m_WalkSpeed;
-    // 移動する方向と速度
-    private Vector3 m_Velocity;
-    // 方向
-    private Vector3 m_Direction;
     // 到着したか判定用
     private bool m_isArrived;
-    // スクリプト
-    private Destination m_Destination;
+    // 現在地
+    private Vector3 m_CurrentPosition;
+    // 目的地
+    private Vector3 m_Destination;
     // Use this for initialization
     void Start ()
     {
-        //// キャラクターコントローラーを取得する
-        //m_EnemyController = GetComponent<CharacterController>();
         // エージェントを取得する
         m_Agent = GetComponent<NavMeshAgent>();
         // アニメーターを取得する
         m_Animator = GetComponent<Animator>();
         // 到着したかの判定用
         m_isArrived = false;
-        // スクリプトを取得する
-        m_Destination = GetComponent<Destination>();
+        // 現在地を設定する
+        m_CurrentPosition = transform.position;
         // 目的地を設定する
-        m_Destination.SetDestination();
+        SetDestination();
         // エージェントの目的地を設定する
-        if (m_Agent.pathStatus != NavMeshPathStatus.PathInvalid){m_Agent.SetDestination(m_Destination.GetDestination()); }
+        if (m_Agent.pathStatus != NavMeshPathStatus.PathInvalid)
+        {
+            m_Agent.SetDestination(m_Destination);
+        }
     }
 
     // Update is called once per frame
@@ -49,14 +44,25 @@ public class EnemyMove : MonoBehaviour
         // アニメーターのSpeedの値を設定する
         m_Animator.SetFloat("Speed", m_Agent.speed);
         // 目的地から現在の位置までの距離が1未満か判定する
-        if (Vector3.Distance(m_Destination.GetDestination(), transform.position) < 0.8f)
+        if (Vector3.Distance(m_Destination, transform.position) < 0.8f)
         {
             // アニメーターのSpeedの値に0を設定する
             m_Animator.SetFloat("Speed", 0.0f);
             // 目的地を設定する
-            m_Destination.SetDestination();
+            SetDestination();
             // エージェントの目的地を設定する
-            m_Agent.SetDestination(m_Destination.GetDestination());
+            m_Agent.SetDestination(m_Destination);
         }
+    }
+
+    // 目的地を設定する
+    public void SetDestination()
+    {
+        // 現在地を設定する
+        m_CurrentPosition = transform.position;
+        // ランダムなVector2の値を取得
+        Vector2 randDestination = Random.insideUnitCircle * 8;
+        // 現在地から計算して目的地を設定する
+        m_Destination = m_CurrentPosition + new Vector3(randDestination.x, 0.0f, randDestination.y);
     }
 }
